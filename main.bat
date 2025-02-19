9a3cbfb7e51cf4f4f58fa9b970f302cd7ca97548
Mihot7 <105102302+Mihot7@users.noreply.github.com>
2025-02-19 19:56:45 +0100

    Added TIPS, changed UI, fixed bugs, code clean up

--- main.bat
+++ main.bat
@@ -1,57 +1,33 @@
-::[Bat To Exe Converter]
-::
-::fBE1pAF6MU+EWHreyHcjLQlHcCmLNGSuUYkx3Mz0+OSLrUwZUfADfonWyrGJbuUL7yU=
-::fBE1pAF6MU+EWHreyHcjLQlHcCmLNGSuUYkx3Mz0+OSLrUwZUfADYZuW3riebuUL7yU=
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCyDJGyX8VAjFDZdRAuWa1eXMpET/+b34OuDp18hcO0odoPU27CLMq4a6UqE
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSjk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpSI=
-::egkzugNsPRvcWATEpSI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+IeA==
-::cxY6rQJ7JhzQF1fEqQJiZksaHErSXA==
-::ZQ05rAF9IBncCkqN+0xwdVsGAlTMbCXqZg==
-::ZQ05rAF9IAHYFVzEqQIdHD9bWwqOM2q+A6dOiA==
-::eg0/rx1wNQPfEVWB+kM9LVsJDCm2GGStCLkT6ez+/aSCukh9
-::fBEirQZwNQPfEVWB+kM9LVsJDCm2GGStCLkT6ez+/Yo=
-::cRolqwZ3JBvQF1fEqQIAOhRTXgWPfG+1RqUT6uH+/euJq0xdUuszdSwIzfSUYOkd/kD2e5M5wxo=
-::dhA7uBVwLU+EWHSwx002PAxVXgHCdEazDroIv6Gb
-::YQ03rBFzNR3SWATE1lAxI3s=
-::dhAmsQZ3MwfNWATE2Us4Jw8DDFbSbj7a
-::ZQ0/vhVqMQ3MEVWAtB9wBhJRLA==
-::Zg8zqx1/OA3MEVWAtB9wBhJRLA==
-::dhA7pRFwIByZRRmm5kM7SA==
-::Zh4grVQjdCKDJH2B50kkJwtoYTCmM3y0Crod7O3poe+fpy0=
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 @echo off
 cls
 title MTDownloader %ver%
-mode 50,20
-set ver=2.1
+mode 65,20
+set ver=3.0
 
-@chcp 65001
-color 90
+@chcp 65001>nul
+cls
+color 09
 if not exist %temp%\MTDOWNLOAD md %temp%\MTDOWNLOAD
+set "elems[0]=Linki możesz odzielać spacją, aby pobrać parę filmów."
+set "elems[1]=Program jest napędzany przez YT-DLP"
+set "elems[2]=Jeśli program nie działa, zaktualizuj go..."
+set "elems[3]=Program jest portable, odpalisz go z pendrive."
+set "elems[4]=Program zawsze pobiera film w najwyższej dostępnej jakości"
+set "elems[5]=Dzięki za pobranie!"
+
 :menu
+set /a _rand=(%RANDOM% * 6 /32768) 
+cls
 echo Witaj, w programie MTDownloader!
-echo BUILD %ver% - 15.01.2025
+echo Wersja: %ver% - 19.02.2025
+call echo TIP: %%elems[%_rand%]%%
 echo 1. Pobierz film jako dzwięk (MP3)
 echo 2. Pobierz film w formacie MP4
 echo 3. Informacje o programie
 set /p choose=[1,2,3]: 
 if %choose%==1 goto mp3
 if %choose%==2 goto mp4
-if %choose%==3 goto Info
+if %choose%==3 goto info
 cls
 echo Opcja nie znana!!!!
 echo Naciśnij coś!
@@ -66,7 +42,7 @@ echo Pobieranie...
 cd /d %temp%\MTDOWNLOAD
 %~dp0yt-dlp.exe -x --audio-format mp3 --no-warnings --restrict-filenames -q %link% 
 cls
-color 90
+color 09
 echo Pobieranie zakończone!
 echo Naciśnij coś, aby otworzyć folder z pobranym plikiem!
 echo PAMIĘTAJ: PRZENIEŚ GDZIEŚ PLIK!!!! ZOSTANIE ON USUNIĘTY PO ZAMKNIĘCIU PROGRAMU!!!
@@ -84,7 +60,7 @@ echo Pobieranie...
 cd /d %temp%\MTDOWNLOAD
 %~dp0yt-dlp --merge-output-format mp4 -f "bestvideo+bestaudio[ext=m4a]/best" -q %link%
 cls
-color 90
+color 09
 echo Pobieranie zakończone!
 echo Naciśnij coś, aby otworzyć folder z pobranym plikiem!
 echo PAMIĘTAJ: PRZENIEŚ GDZIEŚ PLIK!!!! ZOSTANIE ON USUNIĘTY PO ZAMKNIĘCIU PROGRAMU!!!
@@ -96,11 +72,12 @@ rd %temp%\MTDOWNLOAD /s /q
 exit
 
 :Info
+cls
 echo Wersja programu %ver%
-echo Wersja YT-DLP: 2024.12.23
+echo Wersja YT-DLP: 2025.02.19
 echo Wersja ffmpeg: 7.0
 echo Program na licencji MIT
-echo Program rozwyjany przez:
+echo Program rozwijany przez:
 echo MTSoftware (Mihot7) 2024-2025
 echo Kliknij coś, aby wrócić!
 pause>nul
