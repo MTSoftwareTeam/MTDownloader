29c5a77b47b760bc5a04da1236341ead0523fad7
Mihot7 <105102302+Mihot7@users.noreply.github.com>
2025-06-02 15:20:36 +0200

    Update uninstall.ps1

--- uninstall.ps1
+++ uninstall.ps1
@@ -2,3 +2,15 @@ $desktop = [Environment]::GetFolderPath("Desktop")
 remove-item -fo $Env:LOCALAPPDATA\MTDownloader -recurse
 remove-item -fo "$desktop\MTDownloader.lnk" -recurse
 remove-item -fo ("$Env:APPDATA\Microsoft\Windows\Start Menu\Programs\MTDownloader.lnk") -recurse
+echo Usuwanie opcji odinstalowania...
+
+$key = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\MTDownloader"
+
+# Sprawdź, czy klucz istnieje
+if (Test-Path $key) {
+    # Usuń cały klucz
+    Remove-Item -Path $key -Recurse -Force
+    echo "Klucz rejestru został usunięty."
+} else {
+    echo "Klucz rejestru nie istnieje."
+}
